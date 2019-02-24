69853406f0caae0da810c79ea19a3344876cd227
SJain99 <SJain99@users.noreply.github.com>
2019-02-24 00:30:19 -0500

    cleaning up old branch

--- BackEnd/myapp/server.js
+++ BackEnd/myapp/server.js
@@ -3,7 +3,8 @@ var app = express();
 var trial = 'test';
 
 app.get('/', function(req, res) {
-    res.send(trial) //need data from some function from server-side code to be returned to user here (like some variable that can be referenced).
+    res.send(trial)
+});
 
 app.listen(3000, function() {
     console.log('Listening on port 3000')
