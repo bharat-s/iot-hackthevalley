e3488693a657b86b151d553d0d3a309d2b05d30d
s9ahuja <s9ahuja@uwaterloo.ca>
2019-02-24 06:07:41 -0500

    dataHTV
    arduino, output file and python

---
+++ test.py
@@ -0,0 +1,12 @@
+import serial
+arduinoSerialData = serial.Serial('COM7',9600)
+output = open('outputfile.txt', 'w')
+
+while (1==1):
+    if (arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline()
+        decode = myData.decode('utf-8')
+        print(decode)
+        value = open('outputfile.txt', 'a')
+        value.write(str(decode))
+
